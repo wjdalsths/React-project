9efef8b2c4a76d970053b69a6b45fd33cfb9f0c3
Jeongmin <srm0609@naver.com>
2022-04-13 08:08:16 +0900

    4/13

--- vblog/src/style/global-style.ts
+++ vblog/src/style/global-style.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
    html body {
+    /* width: 1728px;  */
     margin:0;
     padding:0;
     background:#F8F9FA;
